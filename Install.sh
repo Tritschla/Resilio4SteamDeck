c974c743e2a2166d7a1603e3022ebc641f260459
Tritschla <57209817+Tritschla@users.noreply.github.com>
2023-04-08 10:07:06 +0200

    V 1.6
    Instead of creating Uninstall script within install.sh, now it is downloading the file from Git. 
    This is to tighten up the install.sh script and make maintenance of install.sh and UninstallR4SD.sh easier

--- Install.sh
+++ Install.sh
@@ -89,42 +89,9 @@ wget https://raw.githubusercontent.com/Tritschla/Resilio4SteamDeck/main/RepairR4
 chmod +x "$HOME/.R4SD/RepairR4SD.sh"
 
 
-# Creation of Uninstall-R4SD
-cat <<EOF >| "$HOME/.R4SD/UninstallR4SD.sh"
-#!/bin/bash
-
-# Author: Tritschla
-# 2023-04
-
-# Ask the user if they want to uninstall R4SD
-if ! zenity --question --title="Uninstall R4SD?" --text="Are you sure you want to uninstall Resilio for Steam Deck?" --width=300 2> /dev/null ; then
-    exit 0
-fi
-
-# Stop and disable rslsync user
-systemctl --user daemon-reload
-systemctl --user stop rslsync_user
-systemctl --user disable rslsync_user
-
-# Delete rslsync_user.service file
-rm -f "$HOME/.config/systemd/user/rslsync_user.service"
-
-# Recursively delete rslsync & R4SD folder
-rm -rf "$HOME/rslsync"
-rm -rf "$HOME/.R4SD"
-
-# Delete Desktop Icons
-rm -rf "$HOME/Desktop/RepairR4SD.desktop"
-rm -rf "$HOME/Desktop/UninstallR4SD.desktop"
-
-# Delete Start Menue entries
-rm -rf "$HOME"/.local/share/applications/RepairR4SD.desktop
-rm -rf "$HOME"/.local/share/applications/UninstallR4SD.desktop
-
-# Notify the user that the uninstallation is complete
-zenity --info --title="Uninstall R4SD" --text="Resilio for Steam Deck has been successfully uninstalled." --width=300 2> /dev/null
-
-EOF
+# Download of Uninstall-R4SD
+cd "$HOME/.R4SD" || exit 1
+wget https://raw.githubusercontent.com/Tritschla/Resilio4SteamDeck/main/UninstallR4SD.sh
 chmod +x "$HOME/.R4SD/UninstallR4SD.sh"
 
 # Creation of Desktop Shortcuts
